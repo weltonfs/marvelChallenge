ef15f87fa0bb7fe45cc72b0ccfd723faa01fbea4
Welton <weltonfs@gmail.com>
2024-11-05 10:08:42 -0300

    Adding tableview

--- marvelChallange/Views/ MainScreen.swift
+++ marvelChallange/Views/ MainScreen.swift
@@ -12,7 +12,6 @@ protocol ViewDelegate: AnyObject {
 }
 
 class MainScreen: UIView {
-    
     private lazy var label: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
@@ -25,6 +24,16 @@ class MainScreen: UIView {
         button.setTitleColor(.blue, for: .normal)
         button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
         return button
+    }()
+    
+    lazy var tableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .plain)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.separatorStyle = .none
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(CharacterTableViewCell.self, forCellReuseIdentifier: CharacterTableViewCell.identifier)
+        return tableView
     }()
     
     weak var delegate: ViewDelegate?
@@ -49,18 +58,45 @@ class MainScreen: UIView {
     }
 }
 
+extension MainScreen: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        debugPrint("Select Row")
+    }
+}
+
+extension MainScreen: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: CharacterTableViewCell.identifier, for: indexPath) as? CharacterTableViewCell
+        return cell ?? UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 150
+    }
+}
+
 extension MainScreen: ViewCode {
     func addSubviews() {
         addSubview(label)
         addSubview(button)
+        addSubview(tableView)
     }
     
     func setupConstraints() {
         NSLayoutConstraint.activate([
+            label.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
             label.centerXAnchor.constraint(equalTo: centerXAnchor),
-            label.centerYAnchor.constraint(equalTo: centerYAnchor),
             button.topAnchor.constraint(equalTo: label.bottomAnchor, constant: 8),
-            button.centerXAnchor.constraint(equalTo: centerXAnchor)
+            button.centerXAnchor.constraint(equalTo: centerXAnchor),
+            
+            tableView.topAnchor.constraint(equalTo: button.bottomAnchor),
+            tableView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor)
         ])
     }
     
---
+++ marvelChallange/Views/Cells/CharacterTableViewCell.swift
@@ -0,0 +1,40 @@
+import UIKit
+
+class CharacterTableViewCell: UITableViewCell {
+    
+    static let identifier = "CharacterTableViewCell"
+    
+    let cellView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setup()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension CharacterTableViewCell: ViewCode {
+    func addSubviews() {
+        addSubview(cellView)
+    }
+    
+    func setupConstraints() {
+        NSLayoutConstraint.activate([
+            cellView.topAnchor.constraint(equalTo: self.topAnchor),
+            cellView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            cellView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            cellView.bottomAnchor.constraint(equalTo: self.bottomAnchor)
+        ])
+    }
+    
+    func setupStyle() {
+        backgroundColor = .lightGray
+    }
+}
