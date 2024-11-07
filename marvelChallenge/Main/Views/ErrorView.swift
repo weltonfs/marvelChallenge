bded7dd0dbb572001a6f83cf39b7a0a48aa27e7a
Welton <weltonfs@gmail.com>
2024-11-07 20:25:07 -0300

    make errorview

---
+++ marvelChallenge/Main/Views/ErrorView.swift
@@ -0,0 +1,58 @@
+
+import UIKit
+
+class ErrorView: UIView {
+    
+    weak var delegate: ViewDelegate?
+    
+    private lazy var label: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Erro"
+        return label
+    }()
+    
+    private lazy var button: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitleColor(.blue, for: .normal)
+        button.addTarget(self, action: #selector(didTapErrorButton), for: .touchUpInside)
+        button.setTitle("Tentar novamente", for: .normal)
+        return button
+    }()
+
+    init() {
+        super.init(frame: .zero)
+        setup()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    @objc
+    private func didTapErrorButton() {
+        delegate?.didTapErrorButton()
+    }
+}
+
+extension ErrorView: ViewCode {
+    func addSubviews() {
+        addSubview(label)
+        addSubview(button)
+    }
+    
+    func setupConstraints() {
+        NSLayoutConstraint.activate([
+            label.centerXAnchor.constraint(equalTo: centerXAnchor),
+            label.centerYAnchor.constraint(equalTo: centerYAnchor),
+            button.topAnchor.constraint(equalTo: label.bottomAnchor, constant: 8),
+            button.centerXAnchor.constraint(equalTo: centerXAnchor)
+        ])
+    }
+    
+    func setupStyle() {
+        translatesAutoresizingMaskIntoConstraints = false
+        backgroundColor = .white
+    }
+}
