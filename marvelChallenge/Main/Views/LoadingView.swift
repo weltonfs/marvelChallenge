acc72a56dda5341ad24421ba4eba805010cc5a1c
Welton <weltonfs@gmail.com>
2024-11-07 12:49:31 -0300

    adding loading view

--- marvelChallenge/Main/Views/LoadingView.swift
+++ marvelChallenge/Main/Views/LoadingView.swift
@@ -1,7 +1,38 @@
-//
-//  LoadingView.swift
-//  MarvelChallenge
-//
-//  Created by MacBook Pro on 07/11/24.
-//
 
+import UIKit
+
+class LoadingView: UIView {
+    
+    private lazy var activityIndicator: UIActivityIndicatorView = {
+        let ai = UIActivityIndicatorView(style: .large)
+        ai.translatesAutoresizingMaskIntoConstraints = false
+        ai.startAnimating()
+        return ai
+    }()
+    
+    init() {
+        super.init(frame: .zero)
+        setup()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension LoadingView: ViewCode {
+    func addSubviews() {
+        addSubview(activityIndicator)
+    }
+    
+    func setupConstraints() {
+        NSLayoutConstraint.activate([
+            activityIndicator.centerXAnchor.constraint(equalTo: centerXAnchor),
+            activityIndicator.centerYAnchor.constraint(equalTo: centerYAnchor)
+        ])
+    }
+    
+    func setupStyle() {
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
